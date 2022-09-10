b54e3ed2cab7765c2b007940ca5d8afe83f7f535
jumaBrian <njengabrianj@gmail.com>
2022-09-10 22:20:46 +0300

    feat: Add item onclick listener

--- app/src/main/java/com/brayo/movieapp/MainActivity.kt
+++ app/src/main/java/com/brayo/movieapp/MainActivity.kt
@@ -2,8 +2,10 @@ package com.brayo.movieapp
 
 import android.content.res.Configuration
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
@@ -67,19 +69,25 @@ fun MainContent(
     Column(modifier = Modifier.padding(12.dp)) {
         LazyColumn {
             items(items = movieList) {
-                MovieRow(movie = it)
+                MovieRow(movie = it){ movie ->
+                    Log.d("TAG","MainContent: $movie")
+                }
             }
         }
     }
 }
 
 @Composable
-fun MovieRow(movie: String) {
+fun MovieRow(movie: String,
+             onItemClick: (String) -> Unit = {}) {
     Card(
         modifier = Modifier
             .padding(4.dp)
             .fillMaxWidth()
-            .height(130.dp),
+            .height(130.dp)
+            .clickable {
+                       onItemClick(movie)
+            },
         shape = RoundedCornerShape(corner = CornerSize(16.dp)),
         elevation = 6.dp
     ) {

4ec7cd7bb5f82d380151022048d87c3f931b67c1
jumaBrian <njengabrianj@gmail.com>
2022-09-10 21:32:16 +0300

    feat: Enable DarkMode

--- app/src/main/java/com/brayo/movieapp/MainActivity.kt
+++ app/src/main/java/com/brayo/movieapp/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.brayo.movieapp
 
+import android.content.res.Configuration
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -44,7 +45,12 @@ fun MainContent() {
 }
 
 
- @Preview(showBackground = true)
+@Preview(name = "Light Mode")
+@Preview(
+    uiMode = Configuration.UI_MODE_NIGHT_YES,
+    showBackground = true,
+    name = "Dark Mode"
+)
 @Composable
 fun DefaultPreview() {
     MyApp {

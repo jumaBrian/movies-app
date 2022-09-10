79007db6d26c5cb6dbe88b2d2ea2060efa77b538
jumaBrian <njengabrianj@gmail.com>
2022-09-10 23:07:07 +0300

    feat: Instantiate navigation controller

--- app/src/main/java/com/brayo/movieapp/navigation/MovieNavigation.kt
+++ app/src/main/java/com/brayo/movieapp/navigation/MovieNavigation.kt
@@ -1,2 +1,9 @@
 package com.brayo.movieapp.navigation
 
+import androidx.compose.runtime.Composable
+import androidx.navigation.compose.rememberNavController
+
+@Composable
+fun MovieNavigation(){
+    val navController = rememberNavController()
+}
--- app/src/main/java/com/brayo/movieapp/navigation/MovieScreens.kt
+++ app/src/main/java/com/brayo/movieapp/navigation/MovieScreens.kt
@@ -1,2 +1,17 @@
 package com.brayo.movieapp.navigation
 
+// www.google.com/sign_in
+
+enum class MovieScreens {
+    HomeScreen,
+    DetailsScreen;
+
+    companion object {
+        fun fromRoute(route: String?): MovieScreens = when (route?.substringBefore("/")) {
+            HomeScreen.name -> HomeScreen
+            DetailsScreen.name -> DetailsScreen
+            null -> HomeScreen
+            else -> throw IllegalArgumentException("Route $route is not recognized")
+        }
+    }
+}

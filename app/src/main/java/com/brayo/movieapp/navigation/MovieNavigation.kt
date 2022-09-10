b403d6b021fe4096d8fb97bda1e8a647a66abb4f
jumaBrian <njengabrianj@gmail.com>
2022-09-10 23:12:56 +0300

    feat: Configure the navigation host

--- app/src/main/java/com/brayo/movieapp/navigation/MovieNavigation.kt
+++ app/src/main/java/com/brayo/movieapp/navigation/MovieNavigation.kt
@@ -1,9 +1,16 @@
 package com.brayo.movieapp.navigation
 
 import androidx.compose.runtime.Composable
+import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.rememberNavController
 
 @Composable
-fun MovieNavigation(){
+fun MovieNavigation() {
     val navController = rememberNavController()
+    NavHost(
+        navController = navController,
+        startDestination = MovieScreens.HomeScreen.name
+    ) {
+
+    }
 }

64aa0fd01c196d0a7f3990d76d33c634baf84841
jumaBrian <njengabrianj@gmail.com>
2022-09-13 09:19:56 +0300

    feat: Compose Navigation

--- app/src/main/java/com/brayo/movieapp/navigation/MovieNavigation.kt
+++ app/src/main/java/com/brayo/movieapp/navigation/MovieNavigation.kt
@@ -1,8 +1,13 @@
 package com.brayo.movieapp.navigation
 
 import androidx.compose.runtime.Composable
+import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
+import com.brayo.movieapp.screens.HomeScreen
+import com.brayo.movieapp.screens.details.DetailsScreen
 
 @Composable
 fun MovieNavigation() {
@@ -11,6 +16,18 @@ fun MovieNavigation() {
         navController = navController,
         startDestination = MovieScreens.HomeScreen.name
     ) {
+        composable(MovieScreens.HomeScreen.name) {
+            HomeScreen(navController = navController)
+        }
+
+        composable(MovieScreens.DetailsScreen.name + "/{movie}",
+            arguments = listOf(navArgument(name = "movie") { type = NavType.StringType
+            })
+        ) {
+            backStackEntry ->
+            DetailsScreen(navController = navController,
+                backStackEntry.arguments?.getString("movie"))
+        }
 
     }
 }

34dfc18c72d7d80082fb04fd65e26e45ab831a61
jumaBrian <njengabrianj@gmail.com>
2022-09-13 09:21:51 +0300

    feat: Add screen - shows movie details

--- app/src/main/java/com/brayo/movieapp/screens/DetailsScreen.kt
+++ app/src/main/java/com/brayo/movieapp/screens/DetailsScreen.kt
@@ -1,2 +1,27 @@
-package com.brayo.movieapp.screens
+package com.brayo.movieapp.screens.details
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.navigation.NavController
+import java.lang.reflect.Modifier
+
+@Composable
+fun DetailsScreen(navController: NavController, movieData: String?) {
+    Surface(modifier = Modifier.fillMaxHeight()
+        .fillMaxWidth()) {
+        Column(horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center) {
+            Text(text = movieData.toString(), style = MaterialTheme.typography.h5)
+        }
+
+    }
+
+}
 
